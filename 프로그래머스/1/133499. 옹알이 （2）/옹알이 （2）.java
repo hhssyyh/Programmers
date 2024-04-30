dc48f07a4d1f1b5b6816b407af2d9c4708781894
hhssyyh <126137988+hhssyyh@users.noreply.github.com>
2024-04-30 18:13:11 +0900

    [level 1] Title: 옹알이 (2), Time: 0.53 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -6,27 +6,13 @@ class Solution {
         String lastStr;
         
         for(String bab : babbling) {
-            index = 0;
-            lastStr = "";
-            while(true) {
-                if(index==bab.length()) {
-                    answer++;
-                    break;
-                } else if(index<=bab.length()-2
-                   && (bab.substring(index,index+2).equals("ye") || bab.substring(index,index+2).equals("ma"))) {
-                    
-                    if(bab.substring(index,index+2).equals(lastStr)) {break;}
-                    lastStr = bab.substring(index,index+2);
-                    index += 2;
-                } else if(index<=bab.length()-3
-                          && (bab.substring(index,index+3).equals("aya") || bab.substring(index,index+3).equals("woo"))) {
-                    
-                    if(bab.substring(index,index+3).equals(lastStr)) {break;}
-                    lastStr = bab.substring(index,index+3);
-                    index += 3;
-                } else {
-                    break;
-                }
+            if(bab.contains("ayaaya") || bab.contains("yeye") || bab.contains("woowoo") || bab.contains("mama")) {continue;}
+            for(String s : str) {
+                if(bab.contains(s)) {bab = bab.replace(s, " ");}
+            }
+            
+            if(bab.trim().equals("")) {
+                answer++;
             }
         }
         
